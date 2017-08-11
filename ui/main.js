5f4cad50848c08c7816cf08231e06b7e5c457bc2
Tienkoua <30807648+Tienkoua@users.noreply.github.com>
2017-08-11 18:04:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick= function(){
       if(request.readystate===XMLHttpRequest.DONE){
           
           if(request.status==200){
-              var counter=request.response.Text;
+              var counter=request.responseText;
               var span=document.getElementById('count');
               span.innerHTML=counter.toString();
           }
