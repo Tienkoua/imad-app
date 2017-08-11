4297aedf5ec3a025bafe6e505d91f5e600df545a
Tienkoua <30807648+Tienkoua@users.noreply.github.com>
2017-08-11 19:36:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@
 var button=document.getElementById("counter");
 button.onclick= function(){
   //make a request to the counter endpoint
-  var request=new XMLHttpRequest();
+  var request=new XMLhttpRequest();
   
   //capture the response and store it in a variable
   request.onreadystatechange= function(){
