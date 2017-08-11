5cd0b62cce28bffc0693cbcb102245e9a7c7b5bf
Tienkoua <30807648+Tienkoua@users.noreply.github.com>
2017-08-11 17:59:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
-console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+button.onclick= function(){
+  //make a request to the counter endpoint
+  var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange= function(){
+      if(request.readystate===XMLHttpRequest.DONE){
+          
+          if(request.status==200){
+              var counter=request.response.Text;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+  }
+    
+    //make the request
+    request.open('GET','http://mtienkoua.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
+    
+}
